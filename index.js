c5b9ba0ab8bd8ed5a19410ca6274cfa85db2d9d9
luoyangqiu <505072907@qq.com>
2018-03-21 23:47:24 +0800

    'update'

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
         global[name] = factory.apply(this);
     }
 }(this, "site-linkage", function () {
+    "use strict";
     // start
     // console.log('start')
     // import address from './data/address.json'
@@ -16,7 +17,7 @@
     // city 市
     // county 县  //area 区
     // town 镇
-      function address(arguments) {
+      function Address() {
         // console.log(this)
         this.province = null;
         this.provinceList = addressList;
@@ -34,54 +35,58 @@
         // } else {
         //     return addressList;
         // }
-        console.log(this)
-        return this
+        // console.log(this)
     }
-    address.prototype = {
-     getProvince: (province) => {
+    Address.prototype = {
+     getProvince: function(province) {
         console.log('province==>', province)
-        const provinceList = []
+        // const provinceList = []
         this.province = province;
-        this.provinceList.forE        console.log(this)ach(data => {
-            // console.log(data)
-            provinceList.push(data.name)
-        })
-        this.provinceListIndex = provinceList;
-        if (!province) {
-            console.log('没有输入省名')
-        } else {
-            const index = provinceList.indexOf(province);
-            // this.province = province;
-            this.cityList = this.provinceList[index].city;
-            // return this.provinceList[index].city
-        }
+        // console.log(this)
+        // this.provinceList.forEach(data => {
+        //     // console.log(data)
+        //     provinceList.push(data.name)
+        // })
+        // this.provinceListIndex = provinceList;
+        // if (!province) {
+        //     console.log('没有输入省名')
+        // } else {
+        //     const index = provinceList.indexOf(province);
+        //     // this.province = province;
+        //     this.cityList = this.provinceList[index].city;
+        //     // return this.provinceList[index].city
+        // }
         return this
      },
-      getCity: (city) => {
+      getCity: function(city) {
                     console.log('city==>', city)
-                    const cityList = []
-            if (address.province) {
-                address.cityList.forEach(item => {
-                    cityList.push(item.name)
-                })
-                address.cityListIndex = cityList
-                if (city) {
-                    address.city = city
-                    const index = cityList.indexOf(city)
-                    address.areaList = address.cityList[index].area
-                    return address.cityList[index].area
-                } else {
-                    return '没有输入城市名';
-                }
-            } else {
-                // 如果没有输入省名，默认返回所有当前
-                return '没有输入省名';
-            }
+            //         const cityList = []
+            // if (this.province) {
+            //     this.cityList.forEach(item => {
+            //         cityList.push(item.name)
+            //     })
+            //     this.cityListIndex = cityList
+            //     if (city) {
+                    this.city = city
+            //         const index = cityList.indexOf(city)
+            //         this.areaList = this.cityList[index].area
+            //         // return address.cityList[index].area
+            //     } else {
+            //         console.log('没有输入城市名')
+            //         // return '没有输入城市名';
+            //     }
+            // } else {
+            //     // 如果没有输入省名，默认返回所有当前
+            //     // return '没有输入省名';
+            //      console.log('没有输入省名')
+            // }
+            return this
       } 
     }
     // console.log(address)
     // return address
     // window.$linkage = () => {
-        return new address(arguments)
+        const linkage = new Address()
+        return linkage
     // }
 }));
