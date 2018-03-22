97a6faac705f3b6a4e101a6b3937080b2fbc0730
luoyangqiu <505072907@qq.com>
2018-03-22 18:29:49 +0800

    'update'

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
         // console.log(this)
         this.province = null;
         this.provinceList = addressList;
-        this.provinceListIndex = [];
+        this.provinceListIndex = null;
         this.city = null;
         this.cityList = null;
         this.cityListIndex = null;
@@ -30,63 +30,101 @@
         this.areaListIndex = null;
         this.county = null;
         this.town = null;
+        this.data = null; // 返回数据
         // if (arguments) {
         //     console.log(arguments)
         // } else {
         //     return addressList;
         // }
-        // console.log(this)
+        console.log('log==for=>', this)
+        // return this.dataList
     }
     Address.prototype = {
+        queryData: function(data, list) {
+            const dataIndexList = []
+            list.forEach(item => {
+                const name = item.name ? item.name : item
+                dataIndexList.push(name)
+            })
+            switch(data) {
+                case 'province':
+                    this.provinceListIndex = dataIndexList
+                    break;
+                case 'city':
+                    this.cityListIndex = dataIndexList
+                    break;
+                case 'area':
+                    this.areaListIndex = dataIndexList
+                    break;
+                default:
+                    console.log('data type error!')
+
+            }
+        },
      getProvince: function(province) {
-        console.log('province==>', province)
         // const provinceList = []
-        this.province = province;
-        // console.log(this)
-        // this.provinceList.forEach(data => {
-        //     // console.log(data)
-        //     provinceList.push(data.name)
-        // })
-        // this.provinceListIndex = provinceList;
-        // if (!province) {
-        //     console.log('没有输入省名')
-        // } else {
-        //     const index = provinceList.indexOf(province);
-        //     // this.province = province;
-        //     this.cityList = this.provinceList[index].city;
-        //     // return this.provinceList[index].city
-        // }
+        if (this.provinceListIndex && this.provinceListIndex.includes(province)){
+            const index = this.provinceListIndex.indexOf(province);
+            // if (this.provinceList[province]) {
+            this.province = province;
+            this.cityList = this.provinceList[index].city
+            this.queryData('city', this.cityList)
+            this.data = this.cityListIndex
+            // console.log(this.cityList)
+            // } else {
+            // } 
+        } else {
+            console.log('province==>', province)
+            this.queryData('province', this.provinceList)
+            this.getProvince(province)
+        }
         return this
      },
       getCity: function(city) {
-                    console.log('city==>', city)
-            //         const cityList = []
-            // if (this.province) {
-            //     this.cityList.forEach(item => {
-            //         cityList.push(item.name)
-            //     })
-            //     this.cityListIndex = cityList
-            //     if (city) {
-                    this.city = city
-            //         const index = cityList.indexOf(city)
-            //         this.areaList = this.cityList[index].area
-            //         // return address.cityList[index].area
-            //     } else {
-            //         console.log('没有输入城市名')
-            //         // return '没有输入城市名';
-            //     }
+        if (this.cityListIndex && this.cityListIndex.includes(city)){
+            const index = this.cityListIndex.indexOf(city);
+            // if (this.provinceList[province]) {
+            this.city = city;
+            this.areaList = this.cityList[index].area
+            this.queryData('area', this.areaList)
+            this.data = this.areaListIndex
+            // console.log(this.areaList)
             // } else {
-            //     // 如果没有输入省名，默认返回所有当前
-            //     // return '没有输入省名';
-            //      console.log('没有输入省名')
-            // }
-            return this
-      } 
+            // } 
+        } else {
+            console.log('city==>', city)
+            this.queryData('city', this.cityList)
+            this.getCity(city)
+        }
+        return this
+      },
+        getArea: function(area) {
+        if (this.areaListIndex && this.areaListIndex.includes(area)){
+            const index = this.areaListIndex.indexOf(area);
+            // if (this.provinceList[province]) {
+            this.area = area;
+            this.data = this.areaListIndex
+            // console.log(this.areaList)
+            // } else {
+            // } 
+        } else {
+            console.log('area==>', area)
+            this.queryData('area', this.areaList)
+        }
+        return this
+      },
+        init: function(){
+            this.queryData('province', this.provinceList)
+            this.data = this.provinceListIndex
+        }
     }
+    // Address.init()
     // console.log(address)
     // return address
     // window.$linkage = () => {
         const linkage = new Address()
+        linkage.init()
         return linkage
+        // return new Address()
     // }
 }));
